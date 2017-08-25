966fe493d3ac7ff0a209a8eaefd4a65d57b51abb
chazmcgrill <charlietaylordev@gmail.com>
2017-08-25 14:30:21 +0100

    Added Second Marks / Tidied Javascript Selectors
    Added marks for each second. Used loops for pug and sass to keep markup
    to a minimum. Made sizes differ for seconds/minutes and hour marks.
    Also tidied the selectors using query selector rather than element
    class selectors.

--- js/app-min.js
+++ js/app-min.js
@@ -1,6 +1,6 @@
-var secHand = document.getElementsByClassName('sec-hand')[0],
-    minHand = document.getElementsByClassName('min-hand')[0],
-    hrsHand = document.getElementsByClassName('hr-hand')[0];
+var secHand = document.querySelector('.sec-hand'),
+    minHand = document.querySelector('.min-hand'),
+    hrsHand = document.querySelector('.hr-hand');
 
 function setClock() {
   var sec = new Date().getSeconds(),
@@ -10,11 +10,11 @@ function setClock() {
       if(hrs > 12) {
         hrs = hrs - 12;
       }
-      hrsAngle = hrs * 30 + min * 0.5;
+      hrs = hrs * 30 + min * 0.5;
 
   secHand.style.transform = 'rotate(' + sec * 6 + 'deg)';
   minHand.style.transform = 'rotate(' + min * 6 + 'deg)';
-  hrsHand.style.transform = 'rotate(' + hrsAngle + 'deg)';
+  hrsHand.style.transform = 'rotate(' + hrs + 'deg)';
 }
 
 setInterval(setClock, 1000);
